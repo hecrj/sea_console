9fcc21043eb843dcec3ac82aa3f83b8222537b73
Héctor <hector0193@gmail.com>
2012-07-19 23:06:54 +0200

    Shell execute() fixed.

--- core/components/shell/Shell.php
+++ core/components/shell/Shell.php
@@ -14,7 +14,7 @@ class Shell
 	public function execute($command, $location = null)
 	{
 		if(! empty($location))
-			$command = sprintf('cd %s', $location) .' && '. $cmd;
+			$command = sprintf('cd %s', $location) .' && '. $command;
 
 		exec($command . ' 2>&1', $data, $error);
 
